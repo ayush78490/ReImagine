3d57cba90777ee9e5be41d2b2f45543fb23c8cad
ayush78490 <tarunsingh78490@gmail.com>
2024-07-05 02:45:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+VANTA.BIRDS({
+    el: "#main",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color1: 0x1c00a2,
+    color2: 0x77ff
+  })
