e9b102c2000ad4dc212ef7df5f1690a78229056d
ayush78490 <tarunsingh78490@gmail.com>
2024-07-07 23:31:13 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -11,15 +11,33 @@ VANTA.BIRDS({
     color2: 0x77ff
   })
 
-  var tl = gsap.timeline({scrollTrigger:{
-    trigger: "#firstCard",
-    start: "0% 90%",
-    end: "50% 50%",
-    scrub: "true",
-    markers: true
-  }})
+  // var tl = gsap.timeline({scrollTrigger:{
+  //   trigger: "#well",
+  //   start: "100% 90%",
+  //   end: "50% 50%",
+  //   scrub: "true",
+  //   markers: true
+  // }})
 
-  tl.to("#firstCard",{
-    top: "115%",
-    left: "5%"
-  })
+  // tl.to("#cards",{
+  //   top: "100%",
+  //   left: "5%"
+  // })
+
+  function createBubble() {
+    const section = document.querySelector(".HeroImg"); // Use querySelector to select the first matching element
+    const createElement = document.createElement("span");
+
+    var size = Math.random() * 60;
+
+    createElement.style.width = size + 'px';
+    createElement.style.height = size + 'px';
+    createElement.style.left = Math.random() * innerWidth + 'px';
+    section.appendChild(createElement);
+
+    setTimeout(() => {
+        createElement.remove();
+    }, 4000);
+}
+
+setInterval(createBubble, 50);
